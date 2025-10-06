5d8f1cb9ab570118be3542503bc742d6c25adb48
Pritesh Tupe <mail@priteshtupe.com>
2025-10-05 20:54:20 -0400

    Add markov-chain post; Add mdx integration; Refactor CommentBox

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { glob } from "astro/loaders"
 import { z, defineCollection } from "astro:content"
 
 const posts = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/collections/posts" }),
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/collections/posts" }),
   schema: z.object({
     title: z.string(),
     date: z.string(),
