87352837d0c364708d1223c48714279b39413155
Pritesh Tupe <mail@priteshtupe.com>
2025-10-23 14:06:06 -0400

    Add "last updated" to notes

---
+++ src/utils/timeAgo.ts
@@ -0,0 +1,27 @@
+export default function timeAgo(input: Date | string) {
+  if (!input) return "Unknown"
+
+  const ranges = {
+    years: 3600 * 24 * 365,
+    months: 3600 * 24 * 30,
+    weeks: 3600 * 24 * 7,
+    days: 3600 * 24,
+    hours: 3600,
+    minutes: 60,
+    seconds: 1,
+  }
+
+  const date = input instanceof Date ? input : new Date(input)
+  const formatter = new Intl.RelativeTimeFormat("en")
+  const secondsElapsed = (date.getTime() - Date.now()) / 1000
+
+  for (const [key, value] of Object.entries(ranges)) {
+    if (value < Math.abs(secondsElapsed)) {
+      const delta = secondsElapsed / value
+      return formatter.format(
+        Math.round(delta),
+        key as unknown as Intl.RelativeTimeFormatUnit,
+      )
+    }
+  }
+}
